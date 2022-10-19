af8ee078f90e2a033e12383f72d6de1a7ff8b2c1
student <sample@bths.edu>
2022-10-19 11:01:01 -0400

    fixed fully

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,6 +1,8 @@
 // TODO
 // public double roundedToHundredth(double toRound)
 // simplify fraction having 2 negatives [ -1/-1 becomes 1, etc], to do with strings
+import java.text.DecimalFormat;
+import java.math.BigDecimal;
 
 public class LinearEquation {
     private double x1;
@@ -15,35 +17,52 @@ public class LinearEquation {
         this.y2 = y2;
     }
 
-    public String twoPoints() {
-        return "The two points are: (" + Math.round(x1) + "," + Math.round(y1) + ") and (" + Math.round(x2) + "," + Math.round(y2) + ")";
+    public double roundedToHundredth(double toRound) {
+        BigDecimal bd = new BigDecimal(toRound);
+        BigDecimal roundedOffBd = bd.setScale(2, java.math.RoundingMode.HALF_EVEN);
+        return roundedOffBd.doubleValue();
     }
 
-    private double yInt() {
+    public String twoPoints() {
+        return "The two points are: (" + removeEnd.format(x1) + "," + removeEnd.format(y1) + ") and (" + removeEnd.format(x2) + "," + removeEnd.format(y2) + ")";
+    }
+
+    private double yIntDouble() {
         double b;
         b = y1 - (((y2 - y1) / (x2 - x1)) * x1);
-        return Math.round(b);
+        return roundedToHundredth(b);
     }
 
-    public double roundedToHundredth(double toRound) {
-        return 1;
+    private String yInt() {
+        return removeEnd.format(yIntDouble());
     }
 
-    private double slope() {
-        return (y2 - y1) / (x2 - x1);
+    DecimalFormat removeEnd = new DecimalFormat("#.##");
+
+    private String slope() {
+        // return roundedToHundredth((y2 - y1) / (x2 - x1));
+        if ((y2 - y1) < 0 && (x2 - x1) < 0 || (x2 - x1) < 0 && (y2 - y1) > 0) {
+            return removeEnd.format(roundedToHundredth((y2 - y1)*-1)) + "/" + removeEnd.format(roundedToHundredth((x2 - x1)*-1));
+        } else {
+            return removeEnd.format(roundedToHundredth(y2 - y1)) + "/" + removeEnd.format(roundedToHundredth(x2 - x1));
+        }
+    }
+
+    private double slopeDouble() {
+        return roundedToHundredth((y2 - y1) / (x2 - x1));
     }
 
     public String lineEquation() {
-        if (slope() != 0) {
-            if (yInt() > 0) {
+        if (slopeDouble() != 0) {
+            if (yIntDouble() > 0) {
                 return "The equation of the line between the two points is: y = " + slope() + "x" + " + " + yInt();
-            } else if (yInt() < 0) {
+            } else if (yIntDouble() < 0) {
                 return "The equation of the line between the two points is: y = " + slope() + "x" + yInt();
             } else {
                 return "The equation of the line between the two points is: y = " + slope() + "x";
             }
         } else {
-            if (yInt() != 0) {
+            if (yIntDouble() != 0) {
                 return "The equation of the line between the two points is: y = " + yInt();
             } else {
                 return "The equation of the line between the two points is: y = 0";
@@ -52,7 +71,7 @@ public class LinearEquation {
     }
 
     public String slopeString() {
-        return "The slope of the line is " + slope();
+        return "The slope of the line is " + removeEnd.format(slopeDouble());
     }
 
     public String yIntString() {
@@ -60,20 +79,20 @@ public class LinearEquation {
     }
 
     private double calculateDistance() {
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return roundedToHundredth(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
     }
 
     public String distanceString() {
-        return "The distance between the 2 points is " + calculateDistance();
+        return "The distance between the 2 points is " + removeEnd.format(calculateDistance());
     }
 
     private double calculatePoint(double x) {
         double y;
-        y = (slope() * x) + yInt();
+        y = (slopeDouble() * x) + yIntDouble();
         return y;
     }
 
     public String calculatePointString(double x) {
-        return "The point on the line is (" + x + "," + calculatePoint(x) + ")";
+        return "The point on the line is (" + removeEnd.format(x) + "," + removeEnd.format(calculatePoint(x)) + ")";
     }
 }
